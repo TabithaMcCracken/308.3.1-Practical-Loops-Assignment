855bc187cc1a0763e204b82857ebad7f21c3db61
Tabitha <tabitha.r.mccracken@gmail.com>
2024-03-18 17:07:08 -0600

    Finished Assignment

--- script.js
+++ script.js
@@ -16,21 +16,52 @@
 
 //Part 2
 
-let j = 55;
-while(true) {
-  let isPrime = true;
-  for (let k = 2; k < j; k++) {
-    if (j % k === 0) {
-      //not prime
-      isPrime = false;
-      break;
+// let j = 55;
+// while(true) {
+//   let isPrime = true;
+//   for (let k = 2; k < j; k++) {
+//     if (j % k === 0) {
+//       //not prime
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(j, "is prime");
+//     break;
+//   }
+//   j++;
+// }
+
+//Part
+let cell = '';
+let row = '';
+let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+for (const char of csvData){
+    if (char !== ',' && char !== '\n'){
+        cell += char;
+        //console.log(cell)
+    } 
+
+    if(char ===','){
+        row+=cell;
+        cell = "";
     }
-  }
-  if (isPrime) {
-    console.log(j, "is prime");
-    break;
-  }
-  j++;
+
+    if(char === `\n`){
+        row += cell;
+        console.log(row);
+        cell = "";
+        row = "";
+    }
+
 }
+if (cell !== '') {
+    row += `${cell}`;
+    console.log(row);
+  }
+
+
+
 
 
